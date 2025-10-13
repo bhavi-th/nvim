7426f4c6258e44c6e670a4c55f8f8e54c004c7d1
Bhavith.S <bhaviths15@gmail.com>
2025-10-13 19:57:03 +0530

    Configured lang support for lua and js

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = {
+          "lua_ls",
+          "vimls",
+          "ts_ls",
+        }
+      }
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+
+      vim.lsp.enable("lua_ls")
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+      vim.lsp.enable("tsserver")
+
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "●",
+          spacing = 2,
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+    end
+  }
+}
